b775e49aa78a160ef791449dcdf5dce1ae133a36
MM12300 <nerggalkore@gmail.com>
2020-04-19 16:35:49 +0200

    images added slick installed and working

--- js/script.js
+++ js/script.js
@@ -28,6 +28,79 @@ $('.accordeon h3').on('click', function () {
 
 });
 
+// Fancybox
+$('[data-fancybox="gallery"]').fancybox({
+    loop:true,
+    arrows: true,
+    buttons: [
+        //"zoom",
+        "share",
+        //"slideShow",
+        //"fullScreen",
+        //"download",
+        "thumbs",
+        "close"
+      ],
+      //   false
+      //   "zoom"
+      //   "fade"
+      //   "zoom-in-out"
+      animationEffect: "zoom-in-out",
+      //   "fade'
+      //   "slide'
+      //   "circular'
+      //   "tube'
+      //   "zoom-in-out'
+      //   "rotate'
+      transitionEffect: "circular"
+});
+
+    //slick
+    $('.slick').slick({
+      arrows:true,
+      autoplay:true,
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            arrows:true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            arrows:true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            arrows:true,
+            dots: true
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
 });
 
 
