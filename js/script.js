90f57bd7a06f1ad5899f552855da89262c99a41e
MM12300 <nerggalkore@gmail.com>
2020-04-25 18:26:36 +0200

    major changes : photo added layout adjusted

--- js/script.js
+++ js/script.js
@@ -57,14 +57,14 @@ $('[data-fancybox="gallery"]').fancybox({
       dots: true,
       infinite: false,
       speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 4,
+      slidesToShow: 5,
+      slidesToScroll: 5,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
+            slidesToShow: 2,
+            slidesToScroll: 2,
             infinite: true,
             arrows:true,
             dots: true
